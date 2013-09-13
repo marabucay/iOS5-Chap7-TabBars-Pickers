10bbbdb76ab56cf9d24d06ec78c6a287585e85a9
=Shaliemar Abucay <shlabucay@gmail.com>
2013-09-13 15:38:12 +0800

    Implementing Dependent Components.

--- Pickers/Pickers/BIDDependentComponentPickerViewController.h
+++ Pickers/Pickers/BIDDependentComponentPickerViewController.h
@@ -7,7 +7,14 @@
 //
 
 #import <UIKit/UIKit.h>
+#define kStateComponent 0
+#define kZipComponent 1
 
 @interface BIDDependentComponentPickerViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UIPickerView *picker;
+@property (strong, nonatomic) NSDictionary *stateZips;
+@property (strong, nonatomic) NSArray *states;
+@property (strong, nonatomic) NSArray *zips;
+- (IBAction) buttonPressed;
 
 @end

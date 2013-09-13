749f7bd84c1425f05792de5102fb8a64983864e7
=Shaliemar Abucay <shlabucay@gmail.com>
2013-09-13 15:16:58 +0800

    Implemented the Double Component Picker.

--- Pickers/Pickers/BIDDoubleComponentPickerViewController.h
+++ Pickers/Pickers/BIDDoubleComponentPickerViewController.h
@@ -5,9 +5,16 @@
 //  Created by Maria Alice C. Lim on 9/13/13.
 //  Copyright (c) 2013 __MyCompanyName__. All rights reserved.
 //
+#define kFillingComponent 0
+#define kBreadComponent 1
 
 #import <UIKit/UIKit.h>
 
 @interface BIDDoubleComponentPickerViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UIPickerView *doublePicker;
+@property(strong,nonatomic) NSArray *fillingTypes;
+@property(strong,nonatomic) NSArray *breadTypes;
+
+-(IBAction)buttonPressed;
 
 @end
